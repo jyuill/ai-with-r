fc7ff6ff675cc2bac74587d895b58436b434bbe7
John Yuill <john@fig4.com>
2025-07-03 16:10:05 -0700

    TRYIng claude

---
+++ chat_01_claude.R
@@ -0,0 +1,94 @@
+# Initial LLM test ----
+# same as chat_01.R except using Claude model
+# load ellmer pkg for LLMs in R ----
+library(ellmer)
+library(tidyverse)
+
+# Create new chat object ####
+# - specify model, among many -> some may require API key
+# - system prompt sets the tone for the conversation
+# - need Open AI key -> have it saved it in .Renviron
+client <- chat_claude(
+  model = "claude-3.5-turbo",
+  system_prompt = "You are a friendly but terse assistant.")
+
+# USAGE ----
+
+# > Simplest ----
+# - chat in console - use with prompts in R Console:
+# 1. in console: live_console(chat)
+# 2. at >>> add prompt > enter
+# 3. answer from LLM will be displayed in console
+# 4. esc when done
+
+# > More advanced ----
+# - chat in R script - use with prompts in R script:
+# - use 'chat' method directly in script
+# - answers appear in console
+client$chat("What is the capital of France?")
+
+prompto <- "What is the population of Edmonton, Canada?"
+client$chat(prompto)
+
+# > Extracting results ----
+# info accumulates in a chat object with the same name as chat and can be accessed there
+# for example: chat$last_turn() will return results from the last prompt in the chat
+# - can get full conversation by running:
+client
+# - last answer is in chat$last_turn(); not sure how best to isolate the answer, but this seems to work:
+client_last <- client$last_turn()
+client_last@text
+# ...or just
+client$last_turn()@text
+# ...or alternatively ...
+client$last_turn()@contents[[1]]@text
+
+# > Programmatic ----
+# set up function to feed prompts and get response
+chat_function <- function(prompt) {
+  chat_prog <- chat_openai(
+  model = "gpt-4.1", 
+  system_prompt = "You are a friendly but terse assistant.")
+  chat_prog$chat(prompt)
+}
+
+# run prompt with function - answers appear in console, not stored anywhere
+prompto <- "What is the population of Vancouver, BC, Canada?"
+chat_function(prompto)
+
+# > Store results ----
+city <- "Penticton, BC, Canada"
+prompto <- paste("What is the population of", city, "?")
+# store results: answer saved in object, also written to console
+chat_results <- chat_function(prompto)
+
+# add echo = FALSE not prevent showing in console
+chat_function <- function(prompt) {
+  chat_prog <- chat_openai(
+    model = "gpt-4.1", 
+    system_prompt = "You are a friendly but terse assistant.",
+    echo = FALSE) # suppresses output in console if not needed
+  chat_prog$chat(prompt)
+}
+
+pop_geo <- 'Canada'
+pop_prompt <- paste("What is the population of", pop_geo, "?")
+chat_results <- chat_function(pop_prompt)
+chat_results
+
+# Numbers only ----
+# Design a function/system prompt for returning only raw numbers
+ChatNumber <- function(prompt) {
+  chat <- chat_openai(
+    model = "gpt-4.1", 
+    system_prompt = "You are a friendly but terse assistant. 
+    For prompts that return a numerical or quantitative answer 
+    please provide only the raw number in digits only, with no abbreviations, 
+    no additional text.",
+    echo = TRUE) # suppresses output in console if not needed
+  chat$chat(prompt)
+}
+
+pop_geo <- 'Germany'
+pop_prompt <- paste("What is the population of", pop_geo, "?")
+chat_results <- ChatNumber(pop_prompt)
--- chat_10_querychat_app/app.R
+++ chat_10_querychat_app/app.R
@@ -1,6 +1,7 @@
 # Querychat for Shiny!
 # copied directly from
-# https://github.com/posit-dev/querychat/blob/main/pkg-r/README.md
+# https://github.com/posit-dev/querychat
+# detailed documentation: https://github.com/posit-dev/querychat/blob/main/pkg-r/README.md
 # querychat is a drop-in component for Shiny that allows users
 #  to query a data frame using natural language. 
 # The results are available as a reactive data frame, 
@@ -13,9 +14,13 @@ library(bslib)
 library(querychat) # install if needed: pak::pak("posit-dev/querychat/pkg-r")
 # - includes ellmer, no need for ellmer!
 
-# 1. Configure querychat. This is where you specify the dataset and can also
-#    override options like:
-#    - tgreeting message, system prompt, model, etc.
+# 1. CONFIGURE querychat. This is where you specify the dataset and can also
+#    add/edit options like:
+#    - greeting message
+#    - system prompt ('extra instructions')
+#    - model (uses system from ellmer)
+#    - API key if needed
+#    - data description
 querychat_config <- querychat_init(mtcars)
 
 ui <- page_sidebar(
